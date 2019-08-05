4393d38d21316005d746d653b2f499bc4e2b88f0
Dmytro Grendach <grendach@gmail.com>
2019-08-06 01:47:32 +0300

    add some demo commands

--- demo-app/src/main.go
+++ demo-app/src/main.go
@@ -1,19 +1,16 @@
 package main
-
 import (
-	"fmt"
-	"net/http"
-	"os"
+ "fmt"
+ "net/http"
+ "os"
 )
-
 func main() {
-	var PORT string
-	if PORT = os.Getenv("PORT"); PORT == "" {
-		PORT = "3001"
-	}
-	name := "EAT.CH"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello %s from path: %s\n", name, r.URL.Path)
-	})
-	http.ListenAndServe(":"+PORT, nil)
+ var PORT string
+ if PORT = os.Getenv("PORT"); PORT == "" {
+  PORT = "3001"
+ }
+ http.HandleFunc("/EATCH", func(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World from path: %s\n", r.URL.Path)
+ })
+ http.ListenAndServe(":" + PORT, nil)
 }
