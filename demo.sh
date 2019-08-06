243f62d5c2bd953adb2258bc403d6108b775b4e8
Dmytro Grendach <dmytro.grendach@just-eat.com>
2019-08-06 21:08:10 +0300

    add gifs and refactor v2

--- demo.sh
+++ demo.sh
@@ -13,7 +13,7 @@ docker run --network=bridge -p 8080:5000 -it docker:demo
 docker run --network=bridge -p 8090:5000 -it docker:demo
 
 #Run docker in none network mode
-docker run --network=none -p 2222:3001 docker:demo
+docker run --network=none -p 8090:5000 docker:demo
 
 #Run docker in host network mode
 docker run --network=host  docker:demo
