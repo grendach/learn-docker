d01d22e6d56c4df78cfea8d3f65abd2d09b7fbe8
Dmytro Grendach <dmytro.grendach@just-eat.com>
2019-08-06 21:07:23 +0300

    add gifs and refactor v1

--- demo.sh
+++ demo.sh
@@ -5,16 +5,15 @@ docker network ls
 # 9f904ee27bf5        none                null
 # cf03ee007fb4        host                host
 
-docker build -t docker-demo:1111 .
-# docker build -t docker-demo:2222 .
-# docker build -t docker-demo:3333 .
+# Build docker image
+docker build -t docker:demo .
 
-#Запуск контейнера в режимі Мосту:
+# Run docker in bridge network mode
 docker run --network=bridge -p 8080:5000 -it docker:demo
 docker run --network=bridge -p 8090:5000 -it docker:demo
 
-#Запуск в режимі Ніякому
-docker run --network=none -p 2222:3001 docker-demo:1111
+#Run docker in none network mode
+docker run --network=none -p 2222:3001 docker:demo
 
-#Запуск в режимі хоста
-docker run --network=host  docker-demo:3333
+#Run docker in host network mode
+docker run --network=host  docker:demo
